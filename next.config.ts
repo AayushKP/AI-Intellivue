20248849b6774c3b8778fdd5f469bd7cf9e0d078
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-04-14 21:37:10 +0530

    modified file

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@codemirror/state": path.resolve("./node_modules/@codemirror/state"),
+      "@codemirror/view": path.resolve("./node_modules/@codemirror/view"),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
